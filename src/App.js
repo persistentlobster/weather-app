01fd86a81424ebe3ef6b2c883fa12f557b2536ef
Michael <mkelley010@gmail.com>
2019-05-21 12:53:22 -0700

    one more test commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, world! Goodbye, world.
+          Hello, world! Goodbye, world. One last test.
         </p>
         <a
           className="App-link"
