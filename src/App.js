0cb468956b7f41e2820dbd14e7a286cd874e612d
Michael <mkelley010@gmail.com>
2019-05-22 15:07:54 -0700

    enable mapquest api

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
+const mapQuestKey = 'AOpagPUEPGJ7MLysAbqVElfofmi9ViWH';
+
 class App extends Component {
-  render() {
-    return (
-        <Navbar/>
-    );
-  }
+    addressToCoordinates = () => {
+        fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=${mapQuestKey}&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500`)
+            .then(response => response.json())
+            .then(data => console.log(data.results[0].locations[0].latLng))
+            .catch(err => console.log(err));
+    };
+
+    render() {
+        return (
+            <div>
+                {this.addressToCoordinates()};
+                <Navbar />
+            </div>
+        );
+    }
 }
 
 export default App;
