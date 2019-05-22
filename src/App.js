d123053de0d26d34265248f946bdaf566c4bfd8f
Michael <mkelley010@gmail.com>
2019-05-22 15:56:02 -0700

    skeleton for searchbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Searchbar from './components/Searchbar/Searchbar';
 import './App.css';
 
 const mapQuestKey = 'AOpagPUEPGJ7MLysAbqVElfofmi9ViWH';
 
 class App extends Component {
-    addressToCoordinates = () => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ''
+        }
+    }
+    handleChange = () => {
         fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=${mapQuestKey}&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500`)
             .then(response => response.json())
             .then(data => console.log(data.results[0].locations[0].latLng))
@@ -15,8 +22,8 @@ class App extends Component {
     render() {
         return (
             <div>
-                {this.addressToCoordinates()};
                 <Navbar />
+                <Searchbar onChange={this.handleChange} />
             </div>
         );
     }
