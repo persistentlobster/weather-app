004513a8d320f448cecb5e9e7b4671168440ef12
Micah Burnett <persistentlobster@gmail.com>
2019-05-28 14:09:35 -0700

    Update WeatherPane GUI

--- src/components/WeatherPane/WeatherPane.js
+++ src/components/WeatherPane/WeatherPane.js
@@ -17,32 +17,37 @@ class WeatherPane extends Component {
         return true;
     };
 
+    toPercent = (decVal) => {
+        return (decVal * 100).toFixed(0);
+    };
+
     handleValidWeather = (weatherData) => {
         return (
-            <div className="center flex flex-column flex-row-l justify-between ph3 ph4-l pv5 w-70">
-                <div className="w-100 w-33-l ph4-l mb5 mb0-l ba">
+            <div className="center flex flex-column flex-row-l justify-between ph3 ph4-l pv5 w-60 ">
+                <div className="w-100 w-33-l ph4-l mb5 mb0-l bg-white br3 mr2">
                     <div className="img">
-                        <Skycons className="icon-size"
+                        <Skycons className="icon-size mt2"
                             color='black'
                             icon={weatherData.currently.icon.toUpperCase().replace(/-/g, '_')}
                             autoplay={true}
                             // style={this.iconSize}
                         />
                     </div>
+                    <p>{weatherData.currently.summary}</p>
                 </div>
-                <div className="w-100 w-33-l ph4-l mb5 mb0-l ba">
-                    <h1 className="foo">{weatherData.currently.temperature.toPrecision(2)}°</h1>
+                <div className="w-100 w-33-l ph4-l mb5 mb0-l bg-white br3 mr2">
+                    <h1 className="mainTemp">{weatherData.currently.temperature.toPrecision(2)}°</h1>
                     <p>Feels like: {weatherData.currently.apparentTemperature.toPrecision(2)}°</p>
                     <p>
                         Hi: {weatherData.daily.data[0].temperatureHigh.toPrecision(2)}°&nbsp;
                         Low: {weatherData.daily.data[0].temperatureLow.toPrecision(2)}°
                     </p>
                 </div>
-                <div className="w-100 w-33-l ph4-l mb5 mb0-l ba">
-                    <p>Example Detail: Value</p>
-                    <p>Example Detail: Value</p>
-                    <p>Example Detail: Value</p>
-                    <p>Example Detail: Value</p>
+                <div className="details w-100 w-33-l ph4-l mb5 mb0-l bg-white br3 mr2">
+                    <p><b>Humidity:</b> {this.toPercent(weatherData.currently.humidity)}%</p>
+                    <p><b>Precipitation:</b> {this.toPercent(weatherData.currently.precipProbability)}%</p>
+                    <p><b>Wind:</b> {weatherData.currently.windSpeed}&nbsp;mph</p>
+                    <p><b>UV Index:</b> {weatherData.currently.uvIndex}</p>
                 </div>
             </div>
         );
