1e8d6117be8a596bccdf58d3ec7f091ca286450b
Micah Burnett <persistentlobster@gmail.com>
2019-05-30 12:05:26 -0700

    Update searchbar design and resize tachyons logo

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -3,19 +3,21 @@ import React from 'react'
 const Searchbar = ({ onChange, onSubmit }) => {
     return (
         <div className="pa4-l">
+            <form class="mw6 center pa4">
                 <fieldset className="cf bn ma0 pa0 w-100 center">
-                    <legend className="pa0 f5 f4-ns mb3 black-80 center">Enter an address</legend>
+                    <legend className="pa0 f5 f4-ns mb3 black-80 tl">Enter an address</legend>
                     <div className="cf">
                         <div className="mb4 mb0-ns flex justify-center">
                             <input placeholder="Ex: Portland, OR"
-                                   className="mw-100 w-100 w5-ns f5 input-reset ba b--black-20 pv3 ph4 border-box mr1"
+                                   className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
                                    onChange={onChange}/>
                             <input type="submit"
-                                   className="input-reset w-100 w-auto-ns bg-black-80 white f5 pv2 pv3-ns ph4 ba b--black-80 bg-hover-mid-gray pointer"
+                                   className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
                                    onClick={onSubmit} />
                         </div>
                     </div>
                 </fieldset>
+            </form>
         </div>
     );
 };
