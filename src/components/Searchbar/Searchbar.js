86b2d27f5ee5fa1327b1825158eef55a938a8dc5
Micah Burnett <persistentlobster@gmail.com>
2019-06-04 16:41:22 -0700

    Move location pin below searchbar as anchor text

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -11,15 +11,16 @@ const Searchbar = ({ onChange, onSubmit, onLocation }) => {
                         <div className="mb4 mb0-ns flex justify-center">
                             <input placeholder="Ex: Portland, OR"
                                    className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
-                                   onChange={onChange}/>
-                            <i className="fas fa-map-marker-alt bg-white f2 pr2 pl1 pt2 red"
-                                    onClick={onLocation}>
-                            </i>
+                                   onChange={onChange}
+                            />
                             <input type="submit"
                                     className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
                                     onClick={onSubmit}
                             />
                         </div>
+                    </div>
+                    <div className="tl">
+                        <a className="fas fa-map-marker-alt f5 pt3 dark-blue no-underline" href="#0" onClick={onLocation}> Use my location</a>
                     </div>
                 </fieldset>
             </form>
